ebd706fa91de3ecc107d7013b171561bf2e92ca9
JoKronk <JoKronkvist@gmail.com>
2023-08-23 00:32:46 +0300

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +18,7 @@ import { SnackbarComponent } from './snackbar/snackbar.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -25,10 +27,12 @@ import { environment } from '../environments/environment';
     ObsRunComponent,
     RunSplitsComponent,
     RunTimerComponent,
-    SnackbarComponent
+    SnackbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
